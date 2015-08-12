050b5644f90606a2d4e686b3fde60f44d0d07c72
Pranavan C. <pran828@gmail.com>
2015-08-12 09:39:59 -0700

    Update MainActivity.java

--- CodeUProject/app/src/main/java/com/codeu/android/codeuproject/MainActivity.java
+++ CodeUProject/app/src/main/java/com/codeu/android/codeuproject/MainActivity.java
@@ -1,12 +1,14 @@
 package com.codeu.android.codeuproject;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity  extends ActionBarActivity implements GameFragment.Callback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,5 +40,13 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onItemSelected(Uri contentUri) {
+        Intent intent = new Intent(this, DetailActivity.class)
+                    .setData(contentUri);
+            startActivity(intent);
     }
 }
